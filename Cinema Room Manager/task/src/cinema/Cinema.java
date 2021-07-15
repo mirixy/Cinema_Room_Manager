ff6f939b68907561f74e308b36b4726a2669ca2b
Miriam <miriam.lucy@outlook.com>
2021-07-15 16:12:36 +0200

    Finally I did it. I think I could make it look cleaner. But I am done for now

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -1,68 +1,121 @@
 package cinema;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Cinema {
+    public static Scanner scanner = new Scanner(System.in);
 
-    public static int calculateRoom(int rows, int seats){
-        int half;
-        int sum;
-        int second;
-        if (seats * rows <= 60) {
-            sum = rows * seats * 10;
+    public static int calculateRoom(int rows, int seats, int rowNr){
+        int half = rows / 2; // divides the cinema in half
+        int sum = 0;
+        int second = rows - half; // finds the middle row
+        boolean lower60 = false;
+        boolean lowerHalf = false;
+        boolean upperHalf = false;
+        if (seats * rows <= 60) { // are there less than 60 seats
+            lower60 = true;
+            System.out.println("lower 60");
+        } else if (rowNr < second && !lower60) {
+            lowerHalf = true;
+            System.out.println("lower half");
+        } else if (rowNr >= second && !lower60) {
+            upperHalf = true;
+            System.out.println("upper half");
         }
-        else {
-            half = rows / 2;
-            sum = half * seats * 10;
-            second = rows - half;
-            sum += second * seats * 8;
+
+        if (lower60 || lowerHalf) {
+            sum = 10;
+        } else if (upperHalf) {
+            sum = 8;
         }
+
+
         return sum;
     }
-
-    public static String makeGrid() {
-        Scanner scanner = new Scanner(System.in);
+    /* Ask for number of rows and seats
+     *   and returns them as an array  */
+    public static int[] init() {
         System.out.println("Enter the number of rows:");
         int rows = scanner.nextInt();
         System.out.println("Enter the number of seats in each row:");
         int seats = scanner.nextInt();
-        String[][] grid = new String[rows][seats];
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                grid[i][j] = scanner.nextLine();
-            }
-        }
-
+        return new int[] {rows, seats };
     }
-
-    public static void arrays () {
-
-    }
-    public static void main(String[] args) {
-        // Write your code here
-        System.out.println("Cinema:");
-        for (int i = 1; i <= 7; i++) {
-            if (i == 1 ) {
-                System.out.println(" " + " 1 2 3 4 5 6 7 8");
-                System.out.println(i + " S S S S S S S S");
-            } else {
-                System.out.println(i + " S S S S S S S S");
-            }
-        }
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the number of rows:");
-        int rows = scanner.nextInt();
-        System.out.println("Enter the number of seats in each row:");
-        int seats = scanner.nextInt();
-        System.out.println("Total income:");
-        System.out.println("$" + calculateRoom(rows, seats));
-
+    /* Ask for the row and seat the person wants to sit */
+    public static int[] init2() {
         int rowNr;
         int seatNr;
         System.out.println("Enter a row number:");
         rowNr = scanner.nextInt();
-        System.out.println("Enter a seat number:");
+        System.out.println("Enter a seat number in that row:");
         seatNr = scanner.nextInt();
+        System.out.println();
+        return new int[] {rowNr, seatNr};
+    }
+    /* This Method is for the second array, where the chosen
+     *  seat and row is marked with a 'B' */
+    public static String[][] chosenGrid(int rows, int seats , int rowNr, int seatNr) {
+        String[][] grid = new String[rows][seats];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (i == rowNr -1 && j == seatNr - 1) {
+                    grid[i][j] = "B";
+                } else {
+                    grid[i][j] = "S";
+                }
+            }
+        }
+        return grid;
+    }
+    /* First instantiation of the grid without markings */
+    public static String[][] makeGrid(int rows, int seats) {
+
+        String[][] grid = new String[rows][seats];
+        for (String[] strings : grid) {
+            Arrays.fill(strings, "S");
+        }
+
+        return grid;
+
+    }
+    /* Prints the grid out everytime this Method gets invoked. */
+    public static void print(String[][] grid) {
+        /* print the matrix */
+        int nr = 1;
+        int nr2 = 1;
+        System.out.println("Cinema:");
+        System.out.print(" ");
+        for (; nr2 <= grid[0].length; nr2++) {
+            System.out.print(" "+ nr2);
+        }
+        System.out.println();
+
+        for (String[] strings : grid) {
+            System.out.print(nr + " ");
+            for (String string : strings) {
+                System.out.print(string + " ");
+
+            }
+            nr++;
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        // Write your code here
+        int[] result = init();
+        System.out.println();
+        print(makeGrid(result[0], result[1]));
+        System.out.println();
+        int[] input2 = init2();
+        print(chosenGrid(result[0], result[1], input2[0], input2[1]));
+        System.out.println();
+        System.out.println("Ticket price: $" + calculateRoom(result[0], result[1], input2[0]));
+
+
+
+
 
     }
 }
